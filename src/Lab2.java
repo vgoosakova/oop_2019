8b51cdf959dac28dd97949e1fa8423e07c094200
vgoosakova <vgoosakova@gmail.com>
2019-02-22 11:53:15 +0200

    7

--- src/Lab2.java
+++ src/Lab2.java
@@ -1,2 +1,25 @@
 public class Lab2 {
+    public static void main(String[] args) {
+
+        final long a = 4;
+        final long b = 5;
+        final int n = 8;
+        final int m = 6;
+        final int C = 2;
+        float S = 0;
+
+
+        if (a <= C && C <= n || b <= 0 && 0 <= m) {
+            System.out.println("Ділення на нуль!!");
+        }
+        else {
+            for (long i = a; i <= n; i++) {
+                for (long j = b; j <= m; j++) {
+                    S = S + (float) (i % j) / (i - C);
+                }
+
+            }
+            System.out.println(S);
+        }
+    }
 }
